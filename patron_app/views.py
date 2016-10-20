b67fdef0186ebe8238801d69a251e35487193173
Yang Luo <hsluoyz@qq.com>
2016-10-20 23:49:09 +0800

    Get users for /users.

--- patron_app/views.py
+++ patron_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+import json
+import os
+
+
+def users(request):
+    if request.method != "GET":
+        return HttpResponse("Unspported HTTP method: " + request.method, content_type="text/html")
+
+    response_data = []
+
+    patron_dir = "J:/etc/patron"
+    files = os.listdir(patron_dir + "/custom_policy")
+    i = 0
+    for f in files:
+        print f
+        tmp_user = {}
+        tmp_user['id'] = f
+        tmp_user['name'] = "user " + str(i)
+        response_data.append(tmp_user)
+        i += 1
+
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
--- patron_rest/urls.py
+++ patron_rest/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from patron_app.views import users
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^users/$', users),
 ]
